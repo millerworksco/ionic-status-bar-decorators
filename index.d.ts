f4fbec6e7455a83a1ff0449a1156d2b902efc98d
Ryan Miller <ryan@ryanandrewmiller.com>
2018-01-18 08:01:55 -0800

    Add missing typings.

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ export as namespace IonicStatusBarDecorators;
 
 export function HideStatusBar(): void;
 
+export function ShowDarkStatusBar(): void;
+
 export function ShowLightStatusBar(): void;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   HideStatusBar: require('./decorators/hide-status-bar'),
+  ShowDarkStatusBar: require('./decorators/show-dark-status-bar'),
   ShowLightStatusBar: require('./decorators/show-light-status-bar')
 }
